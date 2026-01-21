8844eb686d1baf53cf6fbbe15cadb742eae26c79
Elisa Raeli <79461498+Elisa-Ra@users.noreply.github.com>
2026-01-21 16:50:26 +0100

    adds dao + main

--- u4-w3-d3/src/main/java/elisaraeli/Application.java
+++ u4-w3-d3/src/main/java/elisaraeli/Application.java
@@ -1,8 +1,10 @@
 package elisaraeli;
 
 import elisaraeli.dao.EventiDAO;
-import elisaraeli.entities.Evento;
-import elisaraeli.entities.TipoEvento;
+import elisaraeli.dao.LocationDAO;
+import elisaraeli.dao.PartecipazioneDAO;
+import elisaraeli.dao.PersonaDAO;
+import elisaraeli.entities.*;
 import elisaraeli.exceptions.NotFoundException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -11,59 +13,102 @@ import jakarta.persistence.Persistence;
 import java.time.LocalDate;
 
 public class Application {
+
     private static final EntityManagerFactory entityManagerFactory =
             Persistence.createEntityManagerFactory("gestioneeventipu");
 
     public static void main(String[] args) {
-        // Oggetto speciale che gestisce tutte le interazioni con il DB
+
         EntityManager em = entityManagerFactory.createEntityManager();
+
+        // DAO
+        PersonaDAO personaDAO = new PersonaDAO(em);
+        LocationDAO locationDAO = new LocationDAO(em);
+        PartecipazioneDAO partecipazioneDAO = new PartecipazioneDAO(em);
+
+
+        Persona persona = new Persona(
+                "Elisa",
+                "Raeli",
+                "elisa@example.com",
+                LocalDate.of(1997, 1, 1),
+                "F"
+        );
+        personaDAO.save(persona);
+
+
+        Location location = new Location(
+                "Valle dei Templi",
+                "Agrigento"
+        );
+        locationDAO.save(location);
+        EventiDAO eventidao = getEventiDAO(em, location);
+
+        // Prendo un evento salvato per creare la partecipazione
+        Evento evento = eventidao.findById(1); // Evento 1
+
+        Partecipazione partecipazione = new Partecipazione(
+                persona,
+                evento,
+                StatoPartecipazione.CONFERMATA
+        );
+        partecipazioneDAO.save(partecipazione);
+
+        // FIND BY ID
+        try {
+            Evento eventsFromDB = eventidao.findById(1);
+            System.out.println(eventsFromDB.getTitolo());
+        } catch (NotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        //FIND BY ID AND DELETE
+        try {
+            eventidao.findByIdAndDelete(2);
+        } catch (NotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        em.close();
+        entityManagerFactory.close();
+    }
+
+    private static EventiDAO getEventiDAO(EntityManager em, Location location) {
         EventiDAO eventd = new EventiDAO(em);
 
-        Evento evento1 = new Evento("Evento 1",
+        Evento evento1 = new Evento(
+                "Evento 1",
                 LocalDate.of(2026, 1, 20),
                 "Descrizione dell'evento 1",
                 TipoEvento.PUBBLICO,
-                100
+                100,
+                location
         );
-        Evento evento2 = new Evento("Evento 2",
+
+        Evento evento2 = new Evento(
+                "Evento 2",
                 LocalDate.of(2026, 2, 14),
                 "Descrizione dell'evento 2",
                 TipoEvento.PRIVATO,
-                2
+                2,
+                location
         );
-        Evento evento3 = new Evento("Evento 3",
+
+        Evento evento3 = new Evento(
+                "Evento 3",
                 LocalDate.of(2026, 12, 25),
                 "Descrizione dell'evento 3",
                 TipoEvento.PUBBLICO,
-                500
+                500,
+                location
         );
-
-        // ************************************* SAVE *****************************
 
         eventd.save(evento1);
         eventd.save(evento2);
         eventd.save(evento3);
 
-        // ************************************** FIND BY ID **********************
-        try {
-            Evento eventsFromDB = eventd.findById(1);
-            System.out.println(eventsFromDB.getTitolo());
-
-        } catch (NotFoundException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        // ************************************** FIND BY ID AND DELETE **********************
-        try {
-            eventd.findByIdAndDelete(2);
-        } catch (NotFoundException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        // Best Practice. Quando finisco di utilizzare delle risorse come Scanner, EntityManager, EntityManagerFactory,
-        // è sempre consigliato chiuderle
-
-        em.close();
-        entityManagerFactory.close();
+        return eventd;
     }
 }
+
+
